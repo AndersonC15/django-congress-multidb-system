ce1caf5882f15d5d7a6b89f60215c1e5abf73004
AndersonLoop <coellojanderson@gmail.com>
2026-02-07 23:48:54 -0500

    Modify settings.py

--- sistema_academico_prueba/settings.py
+++ sistema_academico_prueba/settings.py
@@ -29,8 +29,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.trycloudflare.com']
 
-# Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
