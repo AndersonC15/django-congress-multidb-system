1d148f386a27c650465ed8848935638fdeb49443
AndersonLoop <coellojanderson@gmail.com>
2026-02-07 23:48:00 -0500

    test: add enrollment validation tests

--- sistema_academico_prueba/middleware.py
+++ sistema_academico_prueba/middleware.py
@@ -1,4 +1,3 @@
-# middleware.py
 class LogRealIPMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
--- sistema_academico_prueba/urls.py
+++ sistema_academico_prueba/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
-from django.shortcuts import redirect  # <--- ¡AGREGA ESTA LÍNEA!
+from django.shortcuts import redirect 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
